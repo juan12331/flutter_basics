11f9b403382ea6d3622c1aa2b219cc8a4f78a493
Juan12331 <juanxadez2@gmail.com>
2024-10-09 21:34:21 -0300

    adicionando rotas e tal

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'pages/tela.dart';
+
 
 void main() {
-  runApp(Container(
-    child: Center(
-      child: Text('hello world?', textDirection: 
-        TextDirection.ltr, 
-        style: TextStyle(color: Colors.white, fontSize: 25.0),),
-    ),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      routes: {
+        '/': (context) => const tela(),
+        // '/login': (context) => const Login(),
+        // '/cadastro': (context) => const Cadastro(),
+        // '/home': (context) => const Home()
+      },
+    );
+  }
 }
---
+++ flutter_basics/lib/pages/tela.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class tela extends StatefulWidget {
+  const tela({super.key});
+
+  @override
+  State<tela> createState() => _telaState();
+}
+
+class _telaState extends State<tela> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
