c69eecab7a305813921d7fc8991b235c52eb4178
Juan12331 <juanxadez2@gmail.com>
2024-10-19 18:42:25 -0300

    19/10

--- flutter_basics/lib/pages/tela.dart
+++ flutter_basics/lib/pages/tela.dart
@@ -1,31 +1,25 @@
 import 'package:flutter/material.dart';
 
 class tela extends StatefulWidget {
-  
-  
   final String title;
 
   const tela({super.key, required this.title});
-
 
   @override
   State<tela> createState() => _telaState();
 }
 
 class _telaState extends State<tela> {
-
-
-  
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child:  Text(
+        child: Text(
           widget.title,
           textDirection: TextDirection.ltr,
           style: TextStyle(color: Colors.white, fontSize: 25.0),
         ),
       ),
     );
-} }
-
+  }
+}
