bd8cd4e8b5faab51405588aebfdec360824449f4
juan12331 <146020152+juan12331@users.noreply.github.com>
2024-10-29 13:52:41 -0300

    Criando janela que muda de cor (muito legal, só pra variar)

--- flutter_basics/lib/pages/home.dart
+++ flutter_basics/lib/pages/home.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+
+void main() {
+  
+}
 
 class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
   State<Home> createState() => _MyWidgetState();
+
 }
 
 class _MyWidgetState extends State<Home> {
   int counter = 0;
+  int counter2 = 0;
+  int counter3 = 0;
+  int counter4 = 0;
+
 
   @override
   Widget build(BuildContext context) {
@@ -18,35 +29,68 @@ class _MyWidgetState extends State<Home> {
         title: Center(
           child: Text(
             "home page",
-             style:TextStyle(
-              fontSize: 25.5,
-              color: Colors.white
-             ) ),
+            style: TextStyle(fontSize: 25.5, color: Colors.white),
+          ),
+        ),
+      ),
+      body: Center( // Centraliza o conteúdo
+        child: Container(
+          width: 300, // Duplicado
+          height: 300, // Duplicado
+          color: Colors.black,
+          child: Center( // Centraliza o stack dentro do container
+            child: Container(
+              width: 240, // Duplicado
+              height: 240, // Duplicado
+              color: Colors.black,
+              child: Stack(
+                children: [
+                  Container(
+                    width: 110, // Duplicado
+                    height: 110, // Duplicado
+                    color:  Color.fromARGB(255, counter3, counter2, counter),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 110, // Duplicado
+                      height: 110, // Duplicado
+                      color:  Color.fromARGB(255, counter, counter2, counter4),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Container(
+                      width: 110, // Duplicado
+                      height: 110, // Duplicado
+                      color:  Color.fromARGB(255, counter3, counter, counter4),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      width: 110, // Duplicado
+                      height: 110, // Duplicado
+                      color: Color.fromARGB(255, counter2, counter3, counter4),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.plus_one),
+        child: Icon(Icons.pan_tool_alt_rounded),
         onPressed: () {
-        setState(() {
-          counter++;
-        }
-        );
-      }),
-      body: Center(
-        child: GestureDetector(
-          child: Text(
-            'contador ${counter}',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
-          ),
-          onTap: () {
-            print('click ${counter}');
-              Navigator.pushNamed(context, "/login");
-            
-          },
-        ),
+          print(counter);
+          setState(() {
+            counter = Random().nextInt(256);
+            counter2 = Random().nextInt(256);
+            counter3 = Random().nextInt(256);
+            counter4 = Random().nextInt(256);
+          });
+        },
       ),
     );
   }
